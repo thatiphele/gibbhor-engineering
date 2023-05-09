18a7a93f7a52342d5abd7f1a6f48b579a0c19f7e
thatiphele <thatayaonephele@gmail.com>
2023-05-09 23:47:27 +0200

    gibbhor engineering website

---
+++ test.js
@@ -0,0 +1,10 @@
+<script>
+$(document).ready(function() {
+///////////////////////////////
+$("#city_id").change(function(){ // change function of listbox
+//alert($('#city_id').val());
+window.location = $('#city_id').val();
+});
+//////////////////////////
+});
+</script>
